ad25ad69158058988e01b183d1c389ea97805839
PMO <pierre@advency.fr>
2023-03-09 19:48:57 +0100

    Sunnyside landing page

--- sunnyside-agency-landing-page-main/src/js/index.js
+++ sunnyside-agency-landing-page-main/src/js/index.js
@@ -1,7 +1,13 @@
 // On page load
 window.onload=function(){
+	// Scroll to content
 	document.getElementById('scroll_to_content').onclick = function() {
 		document.getElementById("page_content").scrollIntoView({behavior: 'smooth'});
 	}
+	// Responsive menu
+	document.getElementById('hamburger').onclick = function() {
+        document.getElementById('hamburger').classList.toggle('open');
+        document.getElementById('main_menu').classList.toggle('open');
+    }
 }
 
