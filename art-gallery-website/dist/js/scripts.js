2c9591bb600e9abbcf634829c7f3c90f81edf41e
PMO <pierre@advency.fr>
2023-03-15 14:42:24 +0100

    WIP artgallery location page

---
+++ art-gallery-website/dist/js/scripts.js
@@ -0,0 +1,21 @@
+// Initialize map container
+var map = L.map('map', {
+	zoomControl: false,
+	dragging: false,
+	scrollWheelZoom: false,
+	doubleClickZoom: false,
+	touchZoom: false,
+}).setView([51.505, -0.09], 13);
+// Add layer
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png').addTo(map);
+// Add marker
+var icon = L.icon({
+    iconUrl: '../assets/img/marker.png',
+    shadowUrl: '../assets/img/marker.png',
+    iconSize:     [66, 88], // size of the icon
+    shadowSize:   [0, 0], // size of the shadow
+    iconAnchor:   [33, 88], // point of the icon which will correspond to marker's location
+    shadowAnchor: [0, 0],  // the same for the shadow
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+});
+var marker = L.marker([51.5, -0.09], {icon: icon}).addTo(map);
