4725244b722e734f932d7b587fb0cd839e1c3e2f
Pierre <pierre@advency.fr>
2023-03-27 17:00:24 +0200

    WIP connect 4 game loop

--- connect-four-game/dist/js/scripts.js
+++ connect-four-game/dist/js/scripts.js
@@ -3,6 +3,10 @@ var gridW = 7;
 var gridH = 6;
 var grid = Array(gridW).fill([...Array(gridH)]);
 var activePlayer = '';
+var defaultTimer = 30;
+var currentTimer = defaultTimer;
+var countdown;
+var stopTimer = false;
 
 window.onload=function(){
 	// Initialize HTML grid
@@ -26,23 +30,78 @@ window.onload=function(){
 	changePlayer();
 
 	// Click event
-	document.querySelectorAll('.grid__col').forEach(item => {
-  		item.addEventListener('click', event => {
-  			var query = item.querySelectorAll('.grid__cell:not(.grid__cell--filled)');
-    		if(!query.length)
-    			return
+	document.querySelectorAll('.grid__col').forEach(gridCol => {
+  		gridCol.addEventListener('click', event => {
+  			// Return if column is filled
+  			if(gridCol.classList.contains('grid__col--filled'))
+  				return false;
+  			// Stop timer
+  			stopTimer = true;
+  			// Prevent click actions on grid
+  			document.body.style.pointerEvents = 'none';
+  			// Get bottom empty cell
+  			var query = gridCol.querySelectorAll('.grid__cell:not(.grid__cell--filled)');
     		var targetCell = query[query.length-1];
-    		targetCell.classList.add('grid__cell--filled');
-    		targetCell.classList.add('grid__cell--' + activePlayer);
-    		changePlayer();
+    		// Create animation
+    		var token = document.createElement('span');
+    		token.classList.add('grid__token');
+    		token.classList.add('grid__token--' + activePlayer);
+    		var cellSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--cell-size'));
+    		var cellIndex = parseInt(targetCell.getAttribute('data-index'));
+    		var speed = Math.max((cellIndex + 1) * 40, 100);
+    		token.style.transitionDuration = speed + 'ms';
+    		gridCol.appendChild(token);
+    		sleep(200)
+    			.then(() => token.style.top = cellSize * cellIndex + 'px')
+    			.then(() => sleep(speed))
+    			.then(() => {
+    				// Fill cell
+		    		targetCell.classList.add('grid__cell--filled');
+		    		targetCell.classList.add('grid__cell--' + activePlayer);
+		    		// Remove token
+		    		gridCol.removeChild(token);
+		    		// If last cell, set column as filled
+		    		if(targetCell.getAttribute('data-index') == 0)
+		    			gridCol.classList.add('grid__col--filled');
+    			})
+    			.then(() => sleep(100))
+    			.then(() => {
+		    		// Change player turn
+		    		changePlayer();
+		  			// Reset click actions on grid
+		  			document.body.style.pointerEvents = 'all';
+    			});
   		})
 	});
 }
 
-function changePlayer() {
-	if(activePlayer != '')
-		document.body.classList.remove(activePlayer);
-	var newPlayer = ['p2', ''].includes(activePlayer) ? 'p1' : 'p2';
-	activePlayer = newPlayer;
-	document.body.classList.add(activePlayer);
+function changePlayer(player = '') {
+	if(player == '')
+		player = getNewPlayer();
+	activePlayer = player;
+	document.body.setAttribute('data-player', activePlayer);
+	// Launch timer
+	launchTimer();
+}
+
+function getNewPlayer() {
+	return ['p2', ''].includes(activePlayer) ? 'p1' : 'p2';
+}
+
+function sleep(ms) {
+  	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function launchTimer() {	
+	clearInterval(countdown);
+	var timerElem = document.getElementById('countdown');
+	currentTimer = defaultTimer;
+	timerElem.innerHTML = currentTimer;
+	stopTimer = false;
+	countdown = setInterval(() => {
+		if(currentTimer == 0 || stopTimer)
+			clearInterval(countdown);
+		currentTimer--;
+		timerElem.innerHTML = currentTimer;
+	}, 1000);	
 }
