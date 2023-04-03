326665ebf3f1602d0f0914ebfb3a205ce5f1ae24
PMO <pierre@advency.fr>
2023-04-03 14:08:05 +0200

    WIP connect 4 game loop

--- connect-four-game/src/js/index.js
+++ connect-four-game/src/js/index.js
@@ -121,6 +121,7 @@ function launchTimer() {
 
 function checkVictory(player, col, cell) {
 	var currentCells = [[col, cell]];
+	var tempCells = [];
 	var processLoop = true;
 	var counter = col;
 
@@ -130,7 +131,7 @@ function checkVictory(player, col, cell) {
 		if(counter < 0 || grid[counter][cell] != player)
 			processLoop = false;
 		else
-			currentCells.push([counter, cell]);
+			tempCells.push([counter, cell]);
 	}
 	// Check line after
 	counter = col;
@@ -140,13 +141,13 @@ function checkVictory(player, col, cell) {
 		if(counter >= gridW || grid[counter][cell] != player)
 			processLoop = false;
 		else
-			currentCells.push([counter, cell]);
+			tempCells.push([counter, cell]);
 	}
-	if(currentCells.length >= 4)
-		return markVictory(currentCells);
+	if(tempCells.length >= 3)
+		currentCells.push(...tempCells);
 
 	// Check column	
-	currentCells = [[col, cell]];
+	tempCells = [];
 	counter = cell;
 	processLoop = true;
 	while(processLoop) {
@@ -154,14 +155,14 @@ function checkVictory(player, col, cell) {
 		if(counter >= gridH || grid[col][counter] != player)
 			processLoop = false;
 		else
-			currentCells.push([col, counter]);
+			tempCells.push([col, counter]);
 	}
-	if(currentCells.length >= 4)
-		return markVictory(currentCells);
+	if(tempCells.length >= 3)
+		currentCells.push(...tempCells);
 
 	// Check diagonals
 	// Bottom left to top right
-	currentCells = [[col, cell]];
+	tempCells = [];
 	currentCol = col;
 	currentCell = cell;
 	processLoop = true;
@@ -172,7 +173,7 @@ function checkVictory(player, col, cell) {
 		if(currentCol < 0 || currentCell >= gridH || grid[currentCol][currentCell] != player)
 			processLoop = false;
 		else
-			currentCells.push([currentCol, currentCell]);
+			tempCells.push([currentCol, currentCell]);
 	}
 	// Check diagonal after
 	currentCol = col;
@@ -184,13 +185,13 @@ function checkVictory(player, col, cell) {
 		if(currentCol >= gridW || currentCell < 0 || grid[currentCol][currentCell] != player)
 			processLoop = false;
 		else
-			currentCells.push([currentCol, currentCell]);
+			tempCells.push([currentCol, currentCell]);
 	}
-	if(currentCells.length >= 4)
-		return markVictory(currentCells);
+	if(tempCells.length >= 3)
+		currentCells.push(...tempCells);
 
 	// Top left to bottom right
-	currentCells = [[col, cell]];
+	tempCells = [];
 	currentCol = col;
 	currentCell = cell;
 	processLoop = true;
@@ -201,7 +202,7 @@ function checkVictory(player, col, cell) {
 		if(currentCol < 0 || currentCell < 0 || grid[currentCol][currentCell] != player)
 			processLoop = false;
 		else
-			currentCells.push([currentCol, currentCell]);
+			tempCells.push([currentCol, currentCell]);
 	}
 	// Check diagonal after
 	currentCol = col;
@@ -213,13 +214,15 @@ function checkVictory(player, col, cell) {
 		if(currentCol >= gridW || currentCell >= gridH || grid[currentCol][currentCell] != player)
 			processLoop = false;
 		else
-			currentCells.push([currentCol, currentCell]);
+			tempCells.push([currentCol, currentCell]);
 	}
+	if(tempCells.length >= 3)
+		currentCells.push(...tempCells);
+
 	if(currentCells.length >= 4)
 		return markVictory(currentCells);
-
-	// Return false if no victory condition is met
-	return false;
+	else
+		return false;
 }
 
 function markVictory(cells) {
